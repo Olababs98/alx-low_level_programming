e992a35110d7cf9d47bf21e36bc10d928c2c2211
Olababs98 <babatundesaheed389@gmail.com>
2022-12-13 10:20:23 -0800

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,19 +1,19 @@
-#include "putchar.c"
+#include "main.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - print _putchar
+ * is written by mbah
+ * Return: 0
  */
 int main(void)
 {
-	char *sh = "putchar";
-
-	while (*sh)
-	{
-		_putchar(*sh);
-		sh++;
-	}
-	_putchar('\n');
-
-	return (0);
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
 }
