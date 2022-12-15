6f07574463327b557d0fc8cc48c1f4e4aa1bff0a
Olababs98 <babatundesaheed389@gmail.com>
2022-12-15 06:06:28 -0800

    101-print_number.c

---
+++ 0x04-more_functions_nested_loops/101-main.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+    print_numbers(98);
+    _putchar('\n');
+    print_numbers(402);
+    _putchar('\n');
+    print_numbers(1024);
+    _putchar('\n');
+    print_number(0);
+    _putchar('\n');
+    print_numbers(-98);
+    _putchar('\n');
+    return (0);
+}
