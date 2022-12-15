0ba754a41ce36f66d66bfd4c417ee6768c5b5d46
Olababs98 <babatundesaheed389@gmail.com>
2022-12-15 11:45:59 -0800

    Numbers have life

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,7 +1,7 @@
 #ifndef main_h
 #define main_h
 
-int _putchar(char);
+int _putchar(char c);
 int _isupper(int c);
 int _isdigit(int c);
 int mul(int a, int b);
@@ -12,5 +12,6 @@ void print_line(int n);
 void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
+void print_number(int n);
 
 #endif
