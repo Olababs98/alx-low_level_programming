6be8189e817991d0cf79676c01ab36dbd9e4cbc9
Olababs98 <babatundesaheed389@gmail.com>
2022-12-15 06:03:37 -0800

    101-print_number.c

--- 0x02-functions_nested_loops/100-main.c
+++ 0x02-functions_nested_loops/100-main.c
@@ -6,12 +6,14 @@
  *
  * Return: Always 0.
  */
-
 int main(void)
 {
-	int n;
-
-	n = add(89, 9);
-	printf("%d\n", n);
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
 	return (0);
 }
