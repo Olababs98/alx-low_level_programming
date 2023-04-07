5b2b61a92489bd0e370eaf4b6c8c798f891afe80
Olababs98 <babatundesaheed389@gmail.com>
2023-04-07 15:29:26 -0700

    hash

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,10 @@
+#include "hash_tables.h"
+
+unsigned long hash_djb2(const unsigned char *str) {
+	unsigned long hash = 5381;
+	int c;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	return hash;
+}
+
